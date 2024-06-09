29c10fa43e357719551595c99cac622f76e781f2
Dhavina Ocxa <103042614+dhavinaocxa@users.noreply.github.com>
2024-06-09 19:05:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_data_from_db():
         host="localhost",  # ganti dengan host database Anda
         user="root",       # ganti dengan user database Anda
         password="",       # ganti dengan password database Anda
-        database="aw"      # ganti dengan nama database Anda
+        database="aw",     # ganti dengan nama database Anda
         port=3306
     )
 
